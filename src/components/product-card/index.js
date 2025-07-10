f4bc510a015de09a3bc3665c5db7f7dc2cd54080
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-11 02:31:24 +0530

    Update index.js

--- src/components/product-card/index.js
+++ src/components/product-card/index.js
@@ -3,11 +3,16 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
 
-function ProductCard({ item }) {
+
+
+function ProductCard({ item }) 
+{
+  
   const router = useRouter();
 
   return (
     <Card>
+    
       <CardContent>
         <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
           <img
@@ -16,24 +21,30 @@ function ProductCard({ item }) {
             className="h-full w-full object-cover object-top"
           />
         </div>
+              
         <div className="p-6">
           <CardTitle className="text-lg font-bold text-gray-900">
             {item?.title}
           </CardTitle>
+            
           <div className="mt-4 flex justify-between items-center flex-wrap gap-2">
             <p className="text-lg font-extrabold text-gray-800">
               {item?.price}
             </p>
             <div>
+              
               <Button onClick={() => router.push(`/${item?.id}`)}>
                 Details
               </Button>
+              
             </div>
           </div>
         </div>
+              
       </CardContent>
     </Card>
   );
-}
+
+} //ProductCard()
 
 export default ProductCard;
