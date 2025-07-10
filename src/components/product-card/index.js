70a8a77be8c78c5163ab29fea91103c44b0eb26a
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-11 02:31:37 +0530

    Update index.js

--- src/components/product-card/index.js
+++ src/components/product-card/index.js
@@ -1,4 +1,6 @@
 "use client";
+
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
