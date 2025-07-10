5ab38e390bea54be57e8d3d733b5fad891cb5a08
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-11 02:07:21 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import ProductCard from "../components/product-card";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function Home() 
+{
+  
   const getSession = await auth();
   console.log(getSession);
 
@@ -14,13 +16,17 @@ export default async function Home() {
   return (
     <div>
       <h1>Shopping Cart</h1>
+    
       <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 max-w-6xl mx-auto p-2">
+
         {getAllProducts && getAllProducts.data && getAllProducts.data.length > 0
           ? getAllProducts.data.map((productItem) => (
               <ProductCard item={productItem} />
             ))
           : null}
+
       </div>
     </div>
   );
-}
+
+} //Home()
