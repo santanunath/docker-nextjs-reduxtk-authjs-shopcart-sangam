67b129275bfdd2728b4c9e8c7a68805f5053ca4a
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-11 12:48:48 +0530

    Update page.js

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,11 +2,14 @@ import { auth } from "@/auth";
 import Cart from "@/components/cart";
 import { redirect } from "next/navigation";
 
-async function CartPage() {
+async function CartPage() 
+{
   const getSession = await auth();
+  
   if (!getSession?.user) redirect("/unauth-page");
 
   return <Cart />;
-}
+    
+} //CartPage()
 
 export default CartPage;
